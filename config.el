245a573022a33cb3be0c6afea48425ee5778b44f
sad <d3v1ant@mail.ru>
2021-11-11 01:02:52 +0300

    set up org-capture tea template

--- config.el
+++ config.el
@@ -151,11 +151,42 @@
   ;; +org-capture-todo-file "tasks.org"
   )
 
+;; org capture templates redefining
+(setq org-time-stamp-formats '("%a <%d-%m-%Y>" . "<%a %d-%m-%Y %H:%M>"))
+
+(setq org-capture-templates
+  '(("a" "New tea" entry
+      (file +org-capture-project-notes-file)
+      "%[~/git/tea/template]" :jump-to-captured t :clock-in t :clock-keep t)
+     ("t" "Personal todo" entry
+      (file +org-capture-todo-file)
+      "* [ ] %?\n%i\n%a" :prepend t)
+     ("n" "Personal notes" entry
+       (file +org-capture-notes-file)
+       "* %u %?\n%i\n%a" :prepend t)
+     ("j" "Journal" entry
+       (file+olp+datetree +org-capture-journal-file)
+       "* %U %?\n%i\n%a" :prepend t)
+     ("p" "Templates for projects")
+     ("pt" "Project-local todo" entry
+       (file +org-capture-project-todo-file)
+       "* TODO %?\n%i\n%a" :prepend t)
+     ("pn" "Project-local notes" entry
+       (file +org-capture-project-notes-file)
+       "* %U %?\n%i\n%a" :prepend t)
+     ("pc" "Project-local changelog" entry
+       (file-headline +org-capture-project-changelog-file "Unreleased")
+       "* %U %?\n%i\n%a" :prepend t)
+     ("o" "Centralized templates for projects")
+     ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+     ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+     ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t))
+  )
 ;; journal setup
 (setq
   org-journal-date-prefix "#+TITLE: "
   org-journal-time-prefix "* "
-  org-journal-date-format "%a, %d-%m-%Y"
+  ;; org-journal-date-format "%a, %d-%m-%Y"
   org-journal-file-format "%Y-%m-%d.org")
 
 (after! org
