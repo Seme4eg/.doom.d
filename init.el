167708fc8cd8bd7175251102958ff08e044ad100
earthian <d3v1ant@mail.ru>
2023-05-25 02:33:33 +0400

    markdown grip don't need

--- init.el
+++ init.el
@@ -127,8 +127,8 @@
   ;;latex             ; writing papers in Emacs has never been so fun
   ;;lean              ; for folks with too much to prove
   ;;ledger            ; be audit you can be
-  (markdown +grip)            ; writing docs for people to ignore
   (lua +lsp +tree-sitter) ; one-based indices? one-based indices
+  (markdown)          ; writing docs for people to ignore
   ;;nim               ; python + lisp at the speed of c
   ;;nix               ; I hereby declare "nix geht mehr!"
   ;;ocaml             ; an objective camel
