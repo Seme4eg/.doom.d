456c5bf01e083648860cf01523436aa8ebd99bbb
earthian <d3v1ant@mail.ru>
2024-01-19 15:22:28 +0300

    docker-podman

--- init.el
+++ init.el
@@ -72,7 +72,7 @@
        ;;collab            ; buffers with friends
        (debugger +lsp)  ; stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       (docker +lsp)
        editorconfig        ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
