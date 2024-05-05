c047e89f1a16079080d370cb5d942c5c01b5ade9
earthian <418@duck.com>
2024-05-05 19:28:48 +0700

    parinfier borked

--- init.el
+++ init.el
@@ -45,7 +45,8 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       parinfer            ; turn lisp into python, sort of
+       ;; FIXME: broken, can't open files from /doom/ dir when have it on
+       ;;parinfer            ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
