38852ddc634dff51acc0ffc3c55676e6f6134b84
earthian <418@duck.com>
2024-09-26 21:26:38 +0700

    feat: drop ligatures

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
        (emoji +github +unicode)    ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
-       (ligatures +extra)  ; ligatures and symbols to make your code pretty again
+       ;; (ligatures +extra)  ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
